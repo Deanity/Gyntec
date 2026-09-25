99bea096ac337c0fbb6087cfb3ca350f305de4c4
Deanity <dendradetama2@gmail.com>
2026-09-25 08:33:01 +0700

    chore: rename APK output to Gyntec.apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,20 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val buildType = variant.buildType.name
+                output.outputFileName = if (buildType == "release") {
+                    "Gyntec.apk"
+                } else {
+                    "Gyntec-$buildType.apk"
+                }
+            }
+    }
 }
 
 flutter {
